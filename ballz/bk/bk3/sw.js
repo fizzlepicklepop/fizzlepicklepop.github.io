e7cec53bd1b12a4a1e04392e9a5b55ab414b921f
Mat <203144840+fizzlepicklepop@users.noreply.github.com>
2025-04-15 03:45:26 -0400

    a

---
+++ ballz/bk/bk3/sw.js
@@ -0,0 +1,34 @@
+const cacheName = 'ballz-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => Promise.all(
+      keyList.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      })
+    ))
+  );
+});
