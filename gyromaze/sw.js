19885c2fea6f1462a6167c46ac4bb5db71335f24
xmat123x <203144840+fizzlepicklepop@users.noreply.github.com>
2026-01-15 20:42:35 -0500

    a

--- gyromaze/sw.js
+++ gyromaze/sw.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = 'gyro-maze-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json'
-];
+const CACHE_NAME = 'gyro-maze-v3-fix';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keys) => Promise.all(
+        keys.map((k) => k !== CACHE_NAME ? caches.delete(k) : null)
+    )));
+    return self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
