60dd90f8b3c4348ecbfbf0a2562c4ff1ea4f206b
xmat123x <203144840+fizzlepicklepop@users.noreply.github.com>
2026-06-13 17:05:58 -0400

    a

--- ipchecker/sw.js
+++ ipchecker/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vpn-ip-check-v13";
+const CACHE_NAME = "vpn-ip-check-v14";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
